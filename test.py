1b9994f10afd399b251409f9c8450f750d4111f2
mss01 <maulikshah1009@gmail.com>
2018-11-07 13:25:23 +0100

    added circumference function to the code

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 print('hello world')
 
+import numpy as np
+
+rad = 1
+
+def circumFer(rad):
+    circ = 2*np.pi*rad
+    return circ
+
+print('Circumference of circle with radius', rad, 'is', circumFer(rad))
+
