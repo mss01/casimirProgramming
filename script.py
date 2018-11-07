f7f1c2edccd5f939e84537ee0d360663d5ec8b07
mss01 <maulikshah1009@gmail.com>
2018-11-07 14:36:22 +0100

    added a docstring to script.py

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ print('Lets use this script to call all our functions from test.py')
 import test
 
 rad2 = 2
-
+'''added a new radius'''
 print('circumference of circle with radius 2 is', test.circumFer(rad2))
 
