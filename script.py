cf83d608d79f3d58377b8b0a0872b70e56a8ca96
mss01 <maulikshah1009@gmail.com>
2018-11-07 15:03:01 +0100

    Resolved a bug for surface area calculation in script.py

--- script.py
+++ script.py
@@ -5,5 +5,5 @@ import test
 rad2 = 2
 '''added a new radius'''
 print('circumference of circle with radius 2 is', test.circumFer(rad2))
-print('Surface area of circle with radius 2 is', test.surfA(rad2))
+print('Surface area of circle with radius 2 is', test.surfaceArea(rad2))
 print('Volume of a Sphere of radius 2 is ', test.VolumeSphere(rad2))
